3fb5cb616f71690aa1e0877883b999b12ed609ab
Anderson Santos <anderson.santos@eunerd.com.br>
2022-04-21 00:31:53 -0300

    Iniciando a configuração do Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
 export function Header(){
+
+    const [isNewTransactionsModalOpena] = useState();
+
     return(
         <Container>
             <Content>
